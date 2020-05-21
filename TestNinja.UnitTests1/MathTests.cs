37722801a084a9d67f840656cd83bf8610018996
nmcdorman <nmcdorman@gmail.com>
2020-05-21 18:43:10 -0400

    S2: #23: Parameterized Testing

---
+++ TestNinja.UnitTests1/MathTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests1
+{
+    [TestFixture]
+    public class MathTests
+    {
+        private TestNinja.Fundamentals.Math _math;
+        //  SetUp
+        //  TearDown
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new TestNinja.Fundamentals.Math();
+
+        }
+
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {            
+            var result = _math.Add(1, 2);
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(1, 1, 1)]
+        public void Max_WhenCalled_ReturnTheGreaterArgument(int a, int b, int expectedResult)
+        {            
+            var result = _math.Max(a, b);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
