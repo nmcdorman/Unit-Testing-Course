cbfef3c3e80b14a1dbb276d91849eecd43258641
nmcdorman <nmcdorman@gmail.com>
2020-05-21 18:59:53 -0400

    S2: 27 Summary - Finished

--- TestNinja.UnitTests1/MathTests.cs
+++ TestNinja.UnitTests1/MathTests.cs
@@ -24,10 +24,11 @@ namespace TestNinja.UnitTests1
         }
 
         [Test]
+        //[Ignore("Because I wanted to!")]
         public void Add_WhenCalled_ReturnTheSumOfArguments()
         {            
             var result = _math.Add(1, 2);
-            Assert.That(result, Is.EqualTo(3));
+            Assert.That(result, Is.EqualTo(3));            
         }
 
         [Test]
