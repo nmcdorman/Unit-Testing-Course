bbbdedfc2c79d3c63290bfe53d9a59c8234b1015
nmcdorman <nmcdorman@gmail.com>
2020-05-21 12:25:45 -0400

    S1 #010 - Using NUnit in Visual Studio

---
+++ TestNinja.UnitTests1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestNinja.UnitTests1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestNinja.UnitTests1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a1c1487b-38d9-4b49-bc9d-710859a743fd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestNinja.UnitTests1/ReservationTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using TestNinja.Fundamentals;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace TestNinja.UnitTests1
+{
+    //  [TestClass] MSTest attribute
+    [TestFixture]   // NUnit framework
+    public class ReservationTests
+    {
+        //  Scenarios when trying to Cancel
+        //  1.  Is an admin
+        //  2.  Is the creator
+        //  3.  3rd party, non-admin or creator
+
+        //[TestMethod]  MSTest attribute
+        [Test]  // NUnit
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            //  1.  Is an admin
+
+            //  Arrange
+            //  where you initialize objects
+            var reservation = new Reservation();
+
+            //  Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            //  Assert  //  Many options for Assert, personal preference
+            //Assert.IsTrue(result);
+            Assert.That(result, Is.True);
+            //Assert.That(result == true);
+        }
+
+        //[TestMethod]  MSTest attribute
+        [Test]  // NUnit
+        public void CanBeCancelledBy_UserIsCreator_ReturnsTrue()
+        {
+            //  2.  Is the creator
+            
+            //  Arrange
+            //  where you initialize objects
+            var creator = new User { IsAdmin = false };
+            var reservation = new Reservation() { MadeBy = creator };            
+
+            //  Act
+            var result = reservation.CanBeCancelledBy(creator);
+
+            //  Assert
+            Assert.IsTrue(result);
+        }
+
+        //[TestMethod]  MSTest attribute
+        [Test]  // NUnit
+        public void CanBeCancelledBy_UserIs3rdParty_ReturnsFalse()
+        {
+            //  3.  3rd party, non-admin or creator
+
+            //  Arrange
+            //  where you initialize objects
+            var reservation = new Reservation() { MadeBy = new User() };  
+            var otherUser = new User { IsAdmin = false };
+            
+            //  Act
+            var result = reservation.CanBeCancelledBy(otherUser);
+
+            //  Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
